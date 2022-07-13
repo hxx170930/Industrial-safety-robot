f9e00cb0f2246ef525917b06b42a1f3a12650392
hxx170930 <3144686146@qq.com>
2022-07-13 11:04:14 +0800

    Industrial-safety-robot

---
+++ onenet1111/applications/oled_display.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2022-06-09     ASUS       the first version
+ */
+#ifndef APPLICATIONS_OLED_DISPLAY_H_
+#define APPLICATIONS_OLED_DISPLAY_H_
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void oled_display(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APPLICATIONS_OLED_DISPLAY_H_ */
+
